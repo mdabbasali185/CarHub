e95f9becfb942273969b1c5a13c73d2f7428800e
Md Abbas Ali <mdabbasali185@gmail.com>
2022-03-25 21:50:27 +0600

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './component/Header/Header';
 
 function App() {
   return (
     <div className="App">
-     
+     <Header></Header>
     </div>
   );
 }
